b0376ac6d60905ae1764e8ead28a2fba1a2f46e2
mnaofficial <mna.official92@gmail.com>
2022-05-22 18:58:32 +0600

    404 page setup. done

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            <h2> 4 0 4 </h2>
+            <img src="https://i.ibb.co/q5F5b5v/404.png" alt="" class="w-full" />
         </div>
     );
 };
