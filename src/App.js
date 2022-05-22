471054338534b91865781becad11acf3ec75069c
mnaofficial <mna.official92@gmail.com>
2022-05-22 13:07:25 +0600

    Banner and navbar setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './Pages/Home/Navbar';
 import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div>
-     <h2 className='text-red-700'>MNA Computer Manufacturer</h2>
-     <Routes>
-       <Route path='/' element={<Home/>}/>
-     </Routes>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+
     </div>
   );
 }
--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div>
-            <h2>Banner</h2>
+        <div class="carousel w-full">
+            <div class="carousel-item relative w-full">
+                <img src="https://i.ibb.co/LPJXGT9/banner1.png" alt="" className='w-full h-80'/>
+            </div>
         </div>
     );
 };
