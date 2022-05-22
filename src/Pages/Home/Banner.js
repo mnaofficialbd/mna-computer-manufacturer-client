697cefb2e30143ec72531be53db8bf36676e4266
mnaofficial <mna.official92@gmail.com>
2022-05-22 20:07:10 +0600

    banner pick convert to slide

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -2,9 +2,29 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div>
-            <img src="https://i.ibb.co/c611zWg/slide-3.png" alt="" className="w-full" />
-        </div>
+            <div class="carousel w-full">
+                <div id="slide1" class="carousel-item relative w-full">
+                    <img src="https://i.ibb.co/c611zWg/slide-3.png" alt="" class="w-full" /> 
+                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" class="btn btn-circle">❮</a>
+                        <a href="#slide2" class="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" class="carousel-item relative w-full">
+                <img src="https://i.ibb.co/C68LZP2/slide-2.png" alt="" className="w-full" />
+                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" class="btn btn-circle">❮</a>
+                        <a href="#slide3" class="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" class="carousel-item relative w-full">
+                <img src="https://i.ibb.co/8bhVqyV/slide-1.png" alt="" className="w-full" />
+                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" class="btn btn-circle">❮</a>
+                        <a href="#slide1" class="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
     );
 };
 
