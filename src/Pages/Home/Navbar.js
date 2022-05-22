d1157801b3fd9466ca346b5890335d40d32dba66
mnaofficial <mna.official92@gmail.com>
2022-05-23 00:21:12 +0600

    firebase.init.js setup

--- src/Pages/Home/Navbar.js
+++ src/Pages/Home/Navbar.js
@@ -13,17 +13,19 @@ const Navbar = () => {
     </>
 
     return (
-        <div class="navbar bg-yellow-600">
+        <div class="navbar bg-yellow-600 shadow-xl">
             <div class="navbar-start">
                 <div class="dropdown">
-                    <label tabIndex="0" class="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    <label tabIndex="0" class="btn btn-circle swap swap-rotate mx-2 lg:hidden">
+                        <input type="checkbox" />
+                        <svg class="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" /></svg>
+                        <svg class="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" /></svg>
                     </label>
                     <ul tabIndex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                         {navLinks}
                     </ul>
                 </div>
-                <Link to='/' class="btn btn-ghost normal-case text-xl">MNA Computer Manufacturer</Link>
+                <Link to='/' class="font-bold text-2xl">MNA Computer Manufacturer</Link>
             </div>
             <div class="navbar-end hidden lg:flex">
                 <ul class="menu menu-horizontal gap-x-2 p-0">
