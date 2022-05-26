caea6f8fc8c3a6067f9193de0f50e547d5c124b4
mnaofficial <mna.official92@gmail.com>
2022-05-26 21:22:57 +0600

    product component and query setup complete

--- src/Pages/Home/OurStock.js
+++ src/Pages/Home/OurStock.js
@@ -2,17 +2,18 @@ import React from 'react';
 
 const OurStock = () => {
     return (
-        <div className='flex justify-center items-center mt-10'>
-            <div className="card lg:max-w-lg bg-base-100 shadow-xl">
+        <div className='justify-center items-center mt-10 mb-12'>
+            <h2 className='text-3xl text-center text-orange-500 font-bold mb-4'>Our Available Product</h2>
+            <div className="card border mx-10 bg-base-100 shadow-xl justify-center items-center">
                 <div className="card-body">
-                <h2 className='text-3xl text-center text-orange-500 font-bold'>Our Available Product</h2>
                     <div >
-                        <div><p>HTML</p><progress class="progress progress-warning w-56" value="1" max="100"></progress></div> <br />
-                        <div><p>HTML</p><progress class="progress progress-warning w-56" value="30" max="100"></progress></div> <br />
-                        <div><p>HTML</p><progress class="progress progress-warning w-56" value="50" max="100"></progress></div> <br />
-                        <div><p>HTML</p><progress class="progress progress-warning w-56" value="80" max="100"></progress></div> <br />
-                        <div><p>HTML</p><progress class="progress progress-warning w-56" value="20" max="100"></progress></div>
-                        
+                        <div><p>Processor</p><progress class="progress progress-warning w-96" value="96" max="100"></progress></div> 
+                        <div><p>Graphics Card</p><progress class="progress progress-warning w-96" value="60" max="100"></progress></div> 
+                        <div><p>Solid State Drive (SSD)</p><progress class="progress progress-warning w-96" value="70" max="100"></progress></div> 
+                        <div><p>Multimedia Keyboard</p><progress class="progress progress-warning w-96" value="40" max="100"></progress></div> 
+                        <div><p>Optical Mouse</p><progress class="progress progress-warning w-96" value="99" max="100"></progress></div>
+                        <div><p>Webcam</p><progress class="progress progress-warning w-96" value="85" max="100"></progress></div>
+
                     </div>
                 </div>
             </div>
